5eaa3779b204be52006dcbda790f37e67cc95e09
ppalekar <ppalekar@LT290.citi-us.com>
2022-10-11 18:57:27 +0530

    new changes

--- TestFramework/src/test/java/com/newtest/TestCases/AAPracticeTest.java
+++ TestFramework/src/test/java/com/newtest/TestCases/AAPracticeTest.java
@@ -1,12 +1,24 @@
 package com.newtest.TestCases;
 
+import java.util.Set;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
+
+import com.newtest.Testing.BaseClass;
 
 public class AAPracticeTest {
 
 	@Test
-public void revUsingForLoop()
+public void scroll()
 {
-		
+String x="ABCDE";
+
+for(int i=x.length()-1; i>=0; i--)
+{
+	System.out.print(x.charAt(i));
 }
+	
+}
+
 }
