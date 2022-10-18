391a1c8e6610c66cf07b6bc085f07034d145cc26
ppalekar <ppalekar@LT290.citi-us.com>
2022-10-18 12:28:31 +0530

    new

--- TestFramework/src/test/java/com/newtest/TestCases/AAPracticeTest.java
+++ TestFramework/src/test/java/com/newtest/TestCases/AAPracticeTest.java
@@ -1,24 +1,37 @@
 package com.newtest.TestCases;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import com.newtest.Testing.BaseClass;
 
-public class AAPracticeTest {
+public class AAPracticeTest{
 
-	@Test
-public void scroll()
+public void takesSceenshot()
 {
-String x="ABCDE";
 
-for(int i=x.length()-1; i>=0; i--)
-{
-	System.out.print(x.charAt(i));
-}
-	
-}
 
+}
 }
